58875e44bd3693fc0bafff84761998b37e2056d0
Chetandev16 <chetanpathak45@gmail.com>
2023-10-15 19:16:58 +0530

    Initilize project/ added auth, db and prisma

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,28 @@
+"use-client";
+
+import { ModeToggle } from "@/components/toggleMode";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  console.log(profile, server);
+  return <div>Crete a server</div>;
+};
+
+export default SetupPage;
